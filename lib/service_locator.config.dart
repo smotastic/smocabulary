a7c463c96c7ed05253b7d12cb955a5d35db4185c
smotastic <nilssmol@gmail.com>
2021-10-08 16:28:34 +0200

    Config is injectable

--- lib/service_locator.config.dart
+++ lib/service_locator.config.dart
@@ -7,8 +7,9 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'core/data/datasources/local/hive/hive_helper.dart' as _i3;
-import 'core/data/datasources/remote/supabase/supabase_helper.dart' as _i4;
+import 'core/data/config.dart' as _i3;
+import 'core/data/datasources/local/hive/hive_helper.dart' as _i4;
+import 'core/data/datasources/remote/supabase/supabase_helper.dart' as _i5;
 
 const String _hive = 'hive';
 // ignore_for_file: unnecessary_lambdas
@@ -17,8 +18,10 @@ const String _hive = 'hive';
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.lazySingleton<_i3.HiveHelper>(() => _i3.HiveHelper(),
+  gh.singletonAsync<_i3.Config>(() => _i3.Config.create());
+  gh.lazySingleton<_i4.HiveHelper>(() => _i4.HiveHelper(),
       registerFor: {_hive});
-  gh.lazySingleton<_i4.SupabaseHelper>(() => _i4.SupabaseHelper());
+  gh.lazySingletonAsync<_i5.SupabaseHelper>(
+      () async => _i5.SupabaseHelper(await get.getAsync<_i3.Config>()));
   return get;
 }
