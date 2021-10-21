2b84d92f9f08ae478942fb1e98f68737f037b0ce
smotastic <nilssmol@gmail.com>
2021-10-21 19:02:32 +0200

    add page

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -5,6 +5,8 @@ abstract class  LocaleKeys {
   static const app = 'app';
   static const feature_courselist_page_title = 'feature.courselist.page_title';
   static const feature_courselist = 'feature.courselist';
+  static const feature_courseadd_page_title = 'feature.courseadd.page_title';
+  static const feature_courseadd = 'feature.courseadd';
   static const feature = 'feature';
 
 }
